00a82231684a91f1f5e2f367377646796e8ea7d9
Amadou Barro <barroama23@gmail.com>
2024-03-28 20:38:10 +0000

    retification et détachement des classes

--- index.php
+++ index.php
@@ -1,14 +1,59 @@
 <?php
 
- class voiture {
-private  $kilometrage;
-private   $marque;
+interface Ireperable {
+    public function réparer();
+  
+}
+
+class Vehicule  implements Ireperable{
+    private $matricule;
+    private $vitesse;
+    public function  __construct($matricule,$vitesse) { 
+        $this->matricule = $matricule;
+        $this->vitesse=$vitesse;
+    }
+
+//accesseur et mutateur matricule
+public function setmatricule ($matricule){
+    $this->matricule=$matricule;
+}
+public function getmatricule(){
+    return  $this->matricule;
+}
+//accesseur et mutateur matricule
+public function setVitesse($vitesse){
+    $this->vitesse=$vitesse;
+}
+public function getVitesse(){
+    return $this->vitesse;
+}
+      public function demarrer(){
+        echo"brm brrmmm bbrrrrmmmmmm";
+      }
+      public function réparer(){
+        echo"Véhicule réparer";
+      }
+
+}
+
+ class voiture extends Vehicule{
+private $kilometrage;
+private $marque;
 private $modele;
 private $annee;
+public function __construct($vitesse,$matricule,$kilometrage,$marque,$modele,$annee )
+{
+    parent::__construct($vitesse,$matricule);
+    $this->kilometrage=$kilometrage;
+    $this->marque=$marque;
+    $this->modele =$modele ;
+    $this-> annee=$annee;
+}
 
 //accesseur et mutateur de kilométrage
 public function setkm ($kilometrage){
     $this->kilometrage=$kilometrage;
+  
 }
 public function getKm() {
     return $this->kilometrage;
@@ -40,29 +85,38 @@ public function getAnnee(){
   
     
   public function afficheVoiture(){
-    echo "Notre voiture est de ". $this->getMarque() ."et a comme modele ".$this->getModele()." et est de ".$this->getAnnee() ."pour". $this->getKm()." kilométre";
+    echo parent::demarrer()." <br> Notre voiture est de ". $this->getMarque() ." et a comme modele ".$this->getModele()." et est de ".$this->getAnnee() ." pour ". $this->getKm()." kilométre ". $this->getVitesse() ." waoh ". $this->getmatricule() ;
   }
-    
+   
+  public function Klaxonne(){
+    echo"<br> pin piinnnn, attention";
+  }
 
+ }
+class moto extends Vehicule {
+    public $electrique;
+    public function __construct($electrique,$vitesse,$matricule)
+   {
+    parent::__construct($vitesse,$matricule);
+    $this->electrique=$electrique;
+   }
+   public function getElectrique(){
+    return $this->electrique;
+   }
+      public function setElectrique($electrique){
+$this->electrique=$electrique;
+   }
+   public function afficheMoto(){
+echo " un nouveau".$this->getElectrique()."a un vitesse". $this->getVitesse()."et est matriculé sous le numéro".$this->getmatricule() ."<br>";
+   }
 
 }
-$voiture1 = new  Voiture();
-$voiture1->setMarque( "Renault");
-$voiture1->setModele("Clio");
-$voiture1->setAnnee("2000");
-$voiture1->setkm(560);
-$voiture1->afficheVoiture();
 
+ $voiture1 = new  Voiture("lumiere","par heure",'525','Renauld','Clio ' ,'2020');
 
-
-
-
-
-
-
-
-
-
-
+ $voiture1 ->afficheVoiture(); //appel de la méthode afficheV
+ $voiture1->Klaxonne();
+$moto1=new moto("moto electrique","789643 ","10km/heure");
+$moto1->afficheMoto();
 
 ?>
